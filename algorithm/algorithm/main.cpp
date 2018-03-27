17961edd0aaaf072c204ad8265acd755dd4bcd8c
Minkyeong <zoz7@naver.com>
2018-03-27 14:08:15 +0900

    11052 : 붕어빵 판매 - 다이나믹 풀이

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -5,36 +5,40 @@
 //  Created by 성민경 on 2017. 11. 27..
 //  Copyright © 2017년 성민경. All rights reserved.
 //
-// 1662
-// 압축
 
 
 #include <iostream>
-#include <algorithm>
+#include <stack>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-
-int main(){
-
-    int a,b,c,d;
+int main() {
     
-    cin >> a >> b >> c >> d;
+    int n;
     
-    string first  = to_string(a) + to_string(b);
-    string second = to_string(c) + to_string(d);
+    cin >> n;
+    vector<int> p(n+1); // 1개 선택할 경우
+    vector<int> MaxPrice(n+1); //
     
-    long long num1 = stoll(first);
-    long long num2 = stoll(second);
+    // 값 입력
+    for(int i=1; i<=n; i++){
+        cin >> p[i];
+    }
     
-    cout << num1 + num2;
+    for(int i=1 ; i<= n; i++){
+        for(int j=1; j<=i; j++){
+            
+            MaxPrice[i] = max(MaxPrice[i], MaxPrice[i-j] + p[j]);
+        }
+    }
+    
+    cout << MaxPrice[n] ;
     
     
     return 0;
 }
-
-
-
 
 
 
