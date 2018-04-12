61fddd1d02bccf80ea744608c2551884ec51dfd4
Minkyeong <zoz7@naver.com>
2018-04-12 14:29:05 +0900

    DFS/BFS

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,58 +1,86 @@
-//
-//  main.cpp
-//  algorithm
-//
-//  Created by 성민경 on 2017. 11. 27..
-//  Copyright © 2017년 성민경. All rights reserved.
-//
-
-
-#include <iostream>
-#include <stack>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
+vector<int> a[1001];
+bool check[1001];
 
-int main() {
+
+void dfs(int x){ // x 를 방문
+
+    check[x] = true;
+    printf("%d ", x);
     
-    string input;
-    int cnt = 0;
-    stack<int> st;
-    
-    cin >> input;
-    
-    int n = input.size();
-    
-    
-    for(int i=0; i<n; i++){
+    for(int i=0; i< a[x].size() ; i++){
         
-        // 쇠막대기 시작
-        if(input[i] == '('){
-            st.push(i);
+        int next = a[x][i];
+        
+        if(check[next] == false){
+            dfs(next);
         }
-        else{// ')'
-            // 막대기 자를 때
-            if(st.top() + 1 == i){
-                st.pop();
-                cnt += st.size();
-            } // 안자른다면 1개씩 막대기가 증가함
-            else{
-                st.pop();
-                cnt++;
+    }
+}
+
+
+void bfs(int x){
+
+    queue<int> q;
+    
+    // 앞에서 check배열을 사용했기때문에 초기화 필요
+    memset(check, false, sizeof(check));
+    
+    // 시작점을 먼저 넣고!
+    check[x] = true;
+    q.push(x);
+    
+    while(!q.empty()){
+    
+        int node = q.front();
+        q.pop();
+        printf("%d ", node);
+        
+        for(int i=0; i< a[x].size(); i++){
+            int next = a[x][i];
+            
+            if(check[next] == false){
+                check[next] = true;
+                q.push(next);
             }
         }
-    
     }
     
-    cout << cnt ;
+
+}
+
+
+int main(){
+
+    
+    int n, m, start;
+    int u, v;
+    
+    scanf("%d %d %d", &n, &m, &start);
     
     
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &u, &v);
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    
+    for(int i=1; i<= n; i++){
+        sort(a[i].begin(), a[i].end());
+    }
+    
+    dfs(start);
+    puts("");
+    bfs(start);
+    puts("");
     
     
     return 0;
 }
-
-
-
