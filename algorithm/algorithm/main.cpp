2124ceb3db2c216c58a5e68a6e0a82bfd69256c9
Minkyeong <zoz7@naver.com>
2018-04-23 17:56:14 +0900

    11726 : 2*n 타일( DP)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,74 +1,32 @@
-#include <cstdio>
-#include <cstring>
-#include <queue>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int arr[11];
-int op[4];
-int maxSum = -1000000000;
-int minSum = 100000000;
-
-
-void dfs(int index, int n, int plus, int minus, int mul, int div, int sum){
-    
-    if(index == n){
-        
-        maxSum = max(maxSum, sum);
-        minSum = min(minSum, sum);
-        return;
-    }
-    
-    
-    
-    if(plus >0){
-        dfs(index+1, n, plus -1 , minus, mul, div, sum + arr[index]);
-    }
-    
-    if(minus >0){
-        dfs(index+1, n, plus  , minus-1, mul, div, sum - arr[index]);
-    }
-    
-    if(mul >0){
-        dfs(index+1, n, plus  , minus, mul-1, div, sum * arr[index]);
-    }
-    
-    if(div >0){
-        dfs(index+1, n, plus , minus, mul, div-1, sum / arr[index]);
-    }
+int n;
+int d[1001] = {0};
 
 
 
-
-
-
-}
 
 int main(){
-    ios::sync_with_stdio(false);
     
+    cin >> n;
     
-    int n;
+    d[0] = 1;
+    d[1] = 1;
     
-    scanf("%d", &n);
-    
-    for(int i=0; i<n; i++){
-        scanf("%d", &arr[i]);
+    // 재귀함수는 시간 복잡도가 커짐 -> 그래서 dp 사용
+    for(int i=2; i<= n; i++){
+        
+        d[i] = (d[i-1] + d[i-2]) % 10007 ;
     }
     
-    for(int j=0; j<4; j++){
-        scanf("%d", &op[j]);
-    }
+    d[n] = d[n] % 10007;
     
-    dfs(1, n, op[0], op[1], op[2], op[3], arr[0]);
+    cout << d[n];
     
-    printf("%d\n%d", maxSum, minSum);
-    
+
     return 0;
-
 }
-
-
-
