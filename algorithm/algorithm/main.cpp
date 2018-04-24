de7e6f54b349bdfdf0e6a36c5ee60839290ada56
Minkyeong <zoz7@naver.com>
2018-04-24 15:21:41 +0900

    11057 : 오르막수(DP)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,55 +1,41 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
 
 using namespace std;
 
-
 int main(){
-    
-    int n;
-    long long dp[101][10]  = {0};
-    long long mod = 1000000000;
-    
+
+    int dp[1001][10] = {0};
+    int n, cnt =0;
     
     cin >> n;
     
     
-    // 길이가 1인 경우
-    for(int i=1; i<10; i++){
+    for(int i=0; i<10; i++){
         dp[1][i] = 1;
     }
     
     for(int i=2; i<=n; i++){
-    
         for(int j=0; j<10; j++){
         
-            // 초기화
-            dp[i][j] = 0;
-            
-            if(j-1 >= 0){
-                dp[i][j] += dp[i-1][j-1];
-            }
-            if(j + 1 <= 9){
-                dp[i][j] += dp[i-1][j+1];
+            int tmp = j;
+            while(tmp>=0){
+                dp[i][j] += (dp[i-1][tmp]);
+                tmp--;
             }
             
-            dp[i][j] = dp[i][j] % mod;
+            dp[i][j] %= 10007;
         }
     }
-    
-    
-    long long cnt = 0;
-    
+
+
+
     for(int i=0; i<10; i++){
         cnt += dp[n][i];
     }
     
-    cnt = cnt % mod ;
+    cnt = cnt % 10007;
     
     cout << cnt << endl;
-    
     
     return 0;
 }
