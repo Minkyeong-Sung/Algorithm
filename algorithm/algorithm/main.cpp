fa33f0751b4475915870922acd703bb7a7ae2d4d
Minkyeong <zoz7@naver.com>
2018-04-25 12:43:18 +0900

    9465 : 스티커(DP[][])

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,41 +1,76 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+
+long long dp[100000][3] = {0};
+long long data[2][100000] = {0};
+
+
+
+
 int main(){
 
-    int dp[1001][10] = {0};
-    int n, cnt =0;
+    int testCase, n;
     
-    cin >> n;
+    cin >> testCase;
     
+    for(int i=0; i<testCase; i++){
     
-    for(int i=0; i<10; i++){
-        dp[1][i] = 1;
-    }
-    
-    for(int i=2; i<=n; i++){
-        for(int j=0; j<10; j++){
+        cin >> n;
+        // input
+        for(int h=0; h<2; h++){
+            for(int j=1; j<= n; j++){
         
-            int tmp = j;
-            while(tmp>=0){
-                dp[i][j] += (dp[i-1][tmp]);
-                tmp--;
+                cin >> data[h][j];
             }
-            
-            dp[i][j] %= 10007;
         }
+        
+        int l=0, k=0;
+        for( l=1; l<= n; l++){
+            for( k=0; k<3; k++){
+            
+            
+                if(k==0){ // X X
+                    dp[l][k] = max(dp[l-1][0], max(dp[l-1][1] , dp[l-1][2]));
+                }
+                
+                if(k==1){ // X O
+                    dp[l][k] = max(dp[l-1][0], dp[l-1][2] ) + data[0][l]  ;
+                }
+                
+                if(k==2){ // O X
+                    dp[l][k] = max(dp[l-1][0] , dp[l-1][1] ) +  data[1][l] ;
+                }
+                
+            }
+        }
+        
+        
+        
+        long long ans = 0;
+        for(int j=1; j<= n; j++){
+            ans = max(dp[j][0], max( dp[j][1], dp[j][2]));
+        }
+        
+        cout << ans << endl;
     }
-
-
-
-    for(int i=0; i<10; i++){
-        cnt += dp[n][i];
-    }
     
-    cnt = cnt % 10007;
     
-    cout << cnt << endl;
-    
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
