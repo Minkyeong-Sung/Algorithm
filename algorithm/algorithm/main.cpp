e8884225769d4dbc8be9f09257b48a30ef3e024a
Minkyeong <zoz7@naver.com>
2018-04-26 14:25:22 +0900

    11054 : 가장 긴 바이토닉 부분 수열(DP)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,76 +1,54 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-
-long long dp[100000][3] = {0};
-long long data[2][100000] = {0};
-
-
-
-
 int main(){
 
-    int testCase, n;
+    int n, ans = 0;
+    int data[1001];
+    int dp_up[1001] = {0};
+    int dp_down[1001] = {0};
     
-    cin >> testCase;
+    cin >> n;
     
-    for(int i=0; i<testCase; i++){
+    for(int i=1; i<= n; i++){
+        cin >> data[i];
+    }
     
-        cin >> n;
-        // input
-        for(int h=0; h<2; h++){
-            for(int j=1; j<= n; j++){
+    for(int i=1; i<= n; i++){
         
-                cin >> data[h][j];
-            }
-        }
-        
-        int l=0, k=0;
-        for( l=1; l<= n; l++){
-            for( k=0; k<3; k++){
+        dp_up[i] = 1;
+        // 증가
+        for(int j=1; j<i; j++){
             
-            
-                if(k==0){ // X X
-                    dp[l][k] = max(dp[l-1][0], max(dp[l-1][1] , dp[l-1][2]));
-                }
-                
-                if(k==1){ // X O
-                    dp[l][k] = max(dp[l-1][0], dp[l-1][2] ) + data[0][l]  ;
-                }
-                
-                if(k==2){ // O X
-                    dp[l][k] = max(dp[l-1][0] , dp[l-1][1] ) +  data[1][l] ;
-                }
+            if(data[i] > data[j]){
+                dp_up[i] = max(dp_up[i], dp_up[j] +1 );
                 
             }
         }
         
-        
-        
-        long long ans = 0;
-        for(int j=1; j<= n; j++){
-            ans = max(dp[j][0], max( dp[j][1], dp[j][2]));
+        // 감소
+        for(int k=n; k>=i; k--){
+            
+            dp_down[k] = 1;
+            
+            for(int j=n; j>k; j--){
+            
+                if(data[k] > data[j]){
+                    
+                    dp_down[k] = max(dp_down[k], dp_down[j]+1);
+                }
+            }
         }
         
-        cout << ans << endl;
+        ans = max(ans, dp_up[i]+ dp_down[i]);
+        
     }
     
     
 
+    cout << ans -1 << endl;
+
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
