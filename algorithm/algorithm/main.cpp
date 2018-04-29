67b1fa5cfb94652df6befe63a9ba56df3aa7b6f5
Minkyeong <zoz7@naver.com>
2018-04-29 17:24:37 +0900

    1912 : 연속합(dp)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -3,51 +3,39 @@
 using namespace std;
 
 int main(){
-
-    int n, ans = 0;
-    int data[1001];
-    int dp_up[1001] = {0};
-    int dp_down[1001] = {0};
+    
+    int n;
+    long long data[100001];
+    long long dp[100001];
     
     cin >> n;
     
-    for(int i=1; i<= n; i++){
+    for(int i=1; i<=n; i++){
         cin >> data[i];
     }
     
-    for(int i=1; i<= n; i++){
+    dp[1] = data[1];
+    
+    for(int i=2; i<= n; i++){
         
-        dp_up[i] = 1;
-        // 증가
-        for(int j=1; j<i; j++){
-            
-            if(data[i] > data[j]){
-                dp_up[i] = max(dp_up[i], dp_up[j] +1 );
-                
-            }
+        dp[i] = data[i];
+        
+        if(dp[i] > dp[i-1] + data[i]){
+            continue;
         }
-        
-        // 감소
-        for(int k=n; k>=i; k--){
-            
-            dp_down[k] = 1;
-            
-            for(int j=n; j>k; j--){
-            
-                if(data[k] > data[j]){
-                    
-                    dp_down[k] = max(dp_down[k], dp_down[j]+1);
-                }
-            }
-        }
-        
-        ans = max(ans, dp_up[i]+ dp_down[i]);
+        dp[i] = max(dp[i], dp[i-1] + data[i]);
         
     }
     
     
+    long long ans = dp[1];
+    for(int i=1; i<= n; i++){
+        ans = max(ans, dp[i]);
+    }
+    
+    cout << ans;
 
-    cout << ans -1 << endl;
+
 
 
     return 0;
