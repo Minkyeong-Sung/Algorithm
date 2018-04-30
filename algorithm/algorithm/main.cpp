49a1e56b160c85941bc9170193a173bc2039ee56
Minkyeong <zoz7@naver.com>
2018-04-30 15:33:39 +0900

    9461 : 파도반 수열 (dp)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -3,38 +3,29 @@
 using namespace std;
 
 int main(){
-    
-    int n;
-    long long data[100001];
-    long long dp[100001];
+
+    long long dp[101] = {0};
+    int n, index=0;
     
     cin >> n;
     
-    for(int i=1; i<=n; i++){
-        cin >> data[i];
-    }
     
-    dp[1] = data[1];
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
     
-    for(int i=2; i<= n; i++){
+    
+    for(int i=0; i<n; i++){
+        cin >> index;
         
-        dp[i] = data[i];
-        
-        if(dp[i] > dp[i-1] + data[i]){
-            continue;
+        for(int j=4; j<= index; j++){
+            
+            dp[j] = dp[j-2] + dp[j-3];
         }
-        dp[i] = max(dp[i], dp[i-1] + data[i]);
+        
+        cout << dp[index] << endl;
         
     }
-    
-    
-    long long ans = dp[1];
-    for(int i=1; i<= n; i++){
-        ans = max(ans, dp[i]);
-    }
-    
-    cout << ans;
-
 
 
 
