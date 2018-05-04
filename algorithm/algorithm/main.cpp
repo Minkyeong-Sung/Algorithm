14ecd6dcc3fd6dc3f355be93c3e26b4e09ca93e7
Minkyeong <zoz7@naver.com>
2018-05-04 16:15:31 +0900

    2004 : 조합0의 개수(팩토리얼 0개수 활용)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,49 +1,43 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+long long int two[3];
+long long int five[3];
+
+void cntNum(long long int n, int flag){
+
+    long long int tmp_two=0, tmp_five=0;
+    long long int i;
+    
+    for(i=2; i<=n ; i *=2){
+        tmp_two += n/i;
+    }
+    
+    for(i=5; i<= n; i *= 5){
+        tmp_five  += n/i;
+    }
+
+    two[flag] = tmp_two;
+    five[flag] = tmp_five;
+}
+
 int main(){
 
-    int n;
-    int check[1000001];
-    int pn=0;
-    int prime[1000000] ;
+    long long int n, m;
+    
+    cin >> n >> m;
+   
+    //  n! / (m! * (n-m)!)
+    cntNum(n, 0);
+    cntNum(m, 1);
+    cntNum(n-m, 2);
+    
+    long long int ans = min(two[0] - two[1] - two[2], five[0] - five[1] - five[2]);
+    
+    cout << ans ;
     
     
-    for(int i = 2; i <= 1000000; i++){
-        
-        if(check[i] == false){
-            
-            prime[pn++] = i;
-        
-            for(int j= i*i; j<= 1000000; j += i){
-                    check[j] = true;
-            }
-        }
-    }
-    
-    check[2] = true;
-    
-    
-    while(1){
-        cin >> n;
-        
-        if(n==0){
-            break;
-        }
-        
-        
-        for(int i=1; i< pn; i++){
-            
-            if(check[n-prime[i]] == false){
-                cout << n << " = " << prime[i] <<  " + " << n-prime[i] << '\n';
-                break;
-            }
-            
-        }
-        
-    }
-
-
     return 0;
 }
