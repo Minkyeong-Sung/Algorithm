ab1b98b7260e63821dc22c9e31685d7b87898d77
Minkyeong <zoz7@naver.com>
2018-07-04 14:05:07 +0900

    11727 : 2*n 타일링2 (DP)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -5,29 +5,23 @@ using namespace std;
 
 int main(){
 
-    int memo[1000001];
+    int memo[1001];
     int input;
     
     cin >> input;
     
-    memo[1] = 0;
+    memo[0] = 1;
+    memo[1] = 1;
     
-    for(int i=2; i<=input; i++)
-    {
-        
-        memo[i] = memo[i-1] + 1;
-        
-        if(i%2 == 0  && memo[i] > memo[i/2] + 1 ){
-            
-            memo[i] = memo[i/2] + 1;
-        }
-        
-        if(i%3 == 0 && memo[i] > memo[i/3] + 1  ){
-            
-            memo[i] = memo[i/3] + 1;
-        }
+    
+    for(int i=2; i<= input; i++){
+      
+        memo[i] = memo[i-1] + 2*memo[i-2];
+    
+        memo[i] %= 10007;
     }
-   
+    
+    
     cout << memo[input];
     
 
