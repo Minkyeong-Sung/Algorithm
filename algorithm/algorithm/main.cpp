f53bd632bd395b85b55cc75b9062808d7e608873
Minkyeong <zoz7@naver.com>
2018-07-06 15:15:26 +0900

    2193 : 이친수(DP)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 
-
 using namespace std;
 
 int main(){
 
-    int memo[1001];
-    int input, n;
+    int n;
+    long long result = 0;
+    long long memo[91][2] = {0};
     
     cin >> n;
     
-    memo[0] = 1;
-    memo[1] = 1;
-    memo[2] = 2;
-    memo[3] = 4;
-
-    for(int i=0; i<n; i++){
-        
-        cin >> input;
-        
-        for(int j=4; j<= input; j++){
-      
-            memo[j] = memo[j-1] + memo[j-2] + memo[j-3];
-        
+    // 한자리인 경우
+    for(int i=0; i<2; i++){
+        // 0으로 시작할 수 없다
+        if(i != 0){
+            memo[1][i] = 1;
         }
-        
-        
-        cout << memo[input] <<'\n';
     }
     
     
+    for(int i=2; i<= n; i++){
+        for(int j=0; j<2; j++){
+         
+            if(j==0){
+                memo[i][j] += memo[i-1][j];
+            
+                memo[i][j] += memo[i-1][j+1];
+            }
+            else{
+                memo[i][j] += memo[i-1][j-1];
+            }
+        }
+    }
+    
+    for(int i=0; i<2; i++){
+        result += memo[n][i];
+    }
+    
+    cout << result;
     
 
     return 0;
