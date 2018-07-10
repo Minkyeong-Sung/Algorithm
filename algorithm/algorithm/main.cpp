c27fdad2efba5b9237a4b7cfe4ba77535d881ae4
Minkyeong <zoz7@naver.com>
2018-07-10 11:54:19 +0900

    1890 : 점프(DP)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
 
-
 using namespace std;
 
 int main(){
 
-    int n, m;
-    int data[1001][1001] = {0};
-    int dp[1001][1001] = {0};
+    int n;
+    long long dp[101][101] = {0};
+    long long data[101][101] = {0};
     
-    cin >> n >> m;
+    cin >>n;
     
     for(int i=1; i<= n; i++){
-        for(int j=1; j<= m; j++){
-            cin >> data[i][j];
+        for(int j=1; j<= n; j++){
+            cin>> data[i][j];
         }
     }
     
+    dp[1][1] = 1;
+    
     for(int i=1; i<= n; i++){
-        for(int j=1; j<= m; j++){
-        
-            dp[i][j] += max(dp[i-1][j] , dp[i][j-1]) + data[i][j];
-         
+        for(int j=1; j<= n; j++){
+            
+            if(data[i][j] != 0){
+            
+                if(j + data[i][j] <= n){
+                    // 오른쪽 이동
+                    dp[i][j + data[i][j]] += dp[i][j];
+                }
+                
+                if(i+ data[i][j] <= n){
+                    // 왼쪽 이동
+                    dp[i+ data[i][j]][j] += dp[i][j];
+                }
+            }
         }
     }
     
-    cout << dp[n][m];
+    cout << dp[n][n] ;
     
-
+    
 
 
 
