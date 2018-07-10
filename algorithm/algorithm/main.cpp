58da2f996a97b897619b434a09c60ed6f602253b
Minkyeong <zoz7@naver.com>
2018-07-10 11:26:07 +0900

    11048 : 이동하기(DP)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,44 +1,36 @@
 #include <iostream>
 
+
 using namespace std;
 
 int main(){
 
-    int n;
-    long long result = 0;
-    long long memo[91][2] = {0};
+    int n, m;
+    int data[1001][1001] = {0};
+    int dp[1001][1001] = {0};
     
-    cin >> n;
+    cin >> n >> m;
     
-    // 한자리인 경우
-    for(int i=0; i<2; i++){
-        // 0으로 시작할 수 없다
-        if(i != 0){
-            memo[1][i] = 1;
+    for(int i=1; i<= n; i++){
+        for(int j=1; j<= m; j++){
+            cin >> data[i][j];
         }
     }
     
-    
-    for(int i=2; i<= n; i++){
-        for(int j=0; j<2; j++){
+    for(int i=1; i<= n; i++){
+        for(int j=1; j<= m; j++){
+        
+            dp[i][j] += max(dp[i-1][j] , dp[i][j-1]) + data[i][j];
          
-            if(j==0){
-                memo[i][j] += memo[i-1][j];
-            
-                memo[i][j] += memo[i-1][j+1];
-            }
-            else{
-                memo[i][j] += memo[i-1][j-1];
-            }
         }
     }
     
-    for(int i=0; i<2; i++){
-        result += memo[n][i];
-    }
+    cout << dp[n][m];
     
-    cout << result;
-    
+
+
+
+
 
     return 0;
 }
