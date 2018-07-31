92209220ee078aaf1229a66569f4c28595d657d5
Minkyeong <zoz7@naver.com>
2018-07-31 19:02:06 +0900

    14503 : 로봇청소기

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,109 +1,82 @@
 #include <iostream>
-#include <cmath>
-#include <cstring>
 
 using namespace std;
 
-int check[101]; // 경사로를 만들었는지 확인
 
+// 0 : 북  1 : 동
+// 2 : 남  3 : 서
 
-// 시작점 , n개, 경사로 길이 , 배열
-int solve(int start, int n,int L, int arr[101][101]){
+int dx[] = {0, -1, 0, 1};
+int dy[] = {-1, 0, 1, 0};
+int changeDir[] = {3, 0, 1, 2};
 
+int bx[] = {1, 0, -1, 0};
+int by[] = {0, -1, 0, 1};
+int cnt, ans;
+
+int solve(int arr[51][51], int x, int y, int dir, int flag){
     
-    for(int i=1; i<n; i++){
+    int tmpX, tmpY;
+    // 청소
+    if(flag == 1){
+        arr[x][y] = 2;
+        ans += 1;
+    }
     
-        // 경사로 차이가 2 이상인 경우
-        if( abs(arr[start][i]-arr[start][i+1]) > 1 ){
+    tmpX = x + dx[dir];
+    tmpY = y + dy[dir];
+    
+    if(cnt > 4){
+        
+        tmpX = x + bx[dir];
+        tmpY = y + by[dir];
+        
+        // 뒤쪽이 벽일 경우
+        if(arr[tmpX][tmpY] == 1){
+            cout << ans;
             return 0;
         }
+        
+        cnt = 1;
+        return solve(arr, tmpX, tmpY, dir, 0);
     
-        int tmp = 0;
-        
-        // 왼쪽이 더 큰 수일 경우
-        if(arr[start][i] > arr[start][i+1]){
-            
-            tmp = arr[start][i+1];
-            
-            // 범위 초과
-            if(i+L > n ) return 0;
-            
-            for(int j=i+1; j<= i+L; j++){
-                
-                // 이미 경사로
-                if(check[j] == -1){
-                    return 0;
-                }
-                
-                if(tmp != arr[start][j]){
-                    return 0;
-                }
-            }
-            
-            // 경사로 표시
-            for(int j=i+1; j <= i + L; j++){
-                check[j] = -1;
-            }
-            i = i + L -1;
-        
-        }
-        else if(arr[start][i] < arr[start][i+1]){
-        
-            if(i-L+1 < 1) return 0;
-            
-            tmp = arr[start][i];
-            
-            for(int j=i; j >= i-L+1; j--){
-                
-                if(check[j] == -1){
-                    return 0;
-                }
-                
-                if(tmp != arr[start][j]){
-                    return 0;
-                }
-            }
-            
-            for(int j=i; j>= i-L+1; j--){
-                check[j] = -1;
-            }
-        
-        }
     }
-
-    return 1;
+    
+    
+    // 2-1
+    if(arr[tmpX][tmpY] == 0){
+        cnt = 1;
+        return solve(arr, tmpX, tmpY, changeDir[dir], 1);
+    }
+    // 2-2
+    else{
+        cnt+=1;
+        return solve(arr, x, y, changeDir[dir] , 0);
+    }
+    
 }
 
 
-
 int main(){
-    
-    int arr[101][101];
-    int reverseArr[101][101];
-    int n, L;
-    
-    cin >> n >> L;
 
-    for(int i=1; i<= n; i++){
-        for(int j=1; j<= n; j++){
+    int n, m;
+    int r, c, d;
+    int arr[51][51];
+    
+    cin >> n >> m;
+    cin >> r >> c >> d;
+    
+    for(int i=0; i< n; i++){
+        for(int j=0; j< m; j++){
             cin >> arr[i][j];
-            reverseArr[j][i] = arr[i][j];
         }
     }
-    
-    int ans = 0;
-    
-    for(int i=1; i<= n; i++){
-        
-        memset(check, 0, sizeof(check));
-        ans += solve(i,n, L, arr);
-        memset(check, 0, sizeof(check));
-        ans += solve(i, n, L, reverseArr);
+    cnt = 1;
+    ans =0;
+    solve(arr, r, c, d, 1);
     
     
-    }
     
-    cout << ans ;
     
     return 0;
 }
