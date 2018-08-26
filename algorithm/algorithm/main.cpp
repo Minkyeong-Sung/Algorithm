d91683300aa942e5ea0512885d8e0811a443468f
Minkyeong <zoz7@naver.com>
2018-08-26 19:33:20 +0900

    2178 : 미로탐색(BFS)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,104 +1,79 @@
 #include <iostream>
 #include <queue>
+#include <cstdio>
 #include <cstring>
 
-using namespace std;
-
-int arr[1001][1001];
-int check[1001][1001];
 int n, m;
-
-queue<pair<int, int>> q;
-
+int arr[101][101];
+int check[101][101];
 
 // 상하좌우
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
-// 정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸
+using namespace std;
 
-void BFS(int i, int j){
 
-    while(!q.empty()){
+// 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸
+
+void BFS(int sx, int sy, int fx, int fy){
+
+    queue<pair<int, int>> q;
     
-        i = q.front().first;
-        j = q.front().second;
+    // 처음 시작 값 입력
+    q.push(make_pair(sx, sy));
+    check[sx][sy] = 1;
+    
+    while(!q.empty()){
+        
+        sx = q.front().first;
+        sy = q.front().second;
         q.pop();
         
-        for(int z = 0; z<4; z++){
+        for(int i=0; i<4; i++){
         
-            int nx = i + dx[z];
-            int ny = j + dy[z];
+            int nx = sx + dx[i];
+            int ny = sy + dy[i];
             
-            if(nx >= 0 && ny >= 0 && nx  < m && ny < n){
-            
-                if(check[nx][ny] == -1 && arr[nx][ny] == 0){
-                    
-                    check[nx][ny] = check[i][j] + 1;
+            // 상하좌우 방향 탐색
+            if(nx >0 && ny >0 && nx <= n &&  ny <= m){
+                
+                // 아직 탐색하지 않았고, 갈 수 있는 길(1) 이라면 더 탐색
+                if(check[nx][ny] == -1 && arr[nx][ny] == 1){
                     q.push(make_pair(nx, ny));
-                    
+                    check[nx][ny] = check[sx][sy] + 1;
                 }
-            
             }
         }
     } // while
+    
+    
 
 }
-
 
 
 int main(){
 
-    
-    // M은 상자의 가로 칸의 수, N 은 상자의 세로 칸의 수
     cin >> n >> m;
     
     memset(check, -1, sizeof(check));
-    memset(arr, 0, sizeof(arr));
     
-    for(int i=0 ; i<m; i++){
-        for(int j=0; j<n; j++){
-            cin >> arr[i][j];
-            
-            if(arr[i][j] == 1){
-                q.push(make_pair(i, j));
-                // 토마토 익는 날짜가 하루씩 낮은 부분을 해결하기 위해 미리 0으로 초기화
-                check[i][j] = 0;
-            }
-        }
-    }
-    
-    
-    BFS(q.front().first, q.front().second);
-    
-    
-    // 토마토 익는 날짜 계산
-    // 체크한 수 중, 가장 큰 수를 최종 익는 날짜로 지정한다.
-    int ans = 0;
-    for(int i=0; i<m; i++){
-        for(int j=0;j<n; j++){
-            
-            if(ans < check[i][j]){
-                ans = check[i][j];
-            }
-        }
-    }
-    
-    for(int i=0 ; i<m; i++){
-        for(int j=0; j<n; j++){
-            // 단, 아직 익지 않은 토마토가 한개라도 있으면 -1
-            if( arr[i][j] == 0 && check[i][j] == -1){
-                ans = -1;
-            }
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            scanf("%1d", &arr[i][j]);
         }
     }
 
+    BFS(1, 1, n, m);
     
-    cout << ans;
-
-
-
+    
+    cout << check[n][m];
 
 
     return 0;
 }
+
+
+
+
+
