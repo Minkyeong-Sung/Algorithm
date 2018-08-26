d8ba2ebaa4de822e8ede47e1d74c05f00b69eba2
Minkyeong <zoz7@naver.com>
2018-08-26 19:23:44 +0900

    7576 : 토마토(BFS)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -2,89 +2,103 @@
 #include <queue>
 #include <cstring>
 
-
 using namespace std;
 
-int arr[50][50];
-int ans[51];
-bool check[50][50];
-int w, h;
+int arr[1001][1001];
+int check[1001][1001];
+int n, m;
 
-// 상하좌우 + 대각선
-int dx[] = {-1, -1, -1,  0, 0, 1, 1, 1};
-int dy[] = {-1, 0, 1, -1, 1, -1,0,  1};
+queue<pair<int, int>> q;
 
-// 1은 땅, 0은 바다
-void BFS(int i, int j, int cnt){
-    
-    queue<pair<int, int>> q;
-    
-    q.push(make_pair(i, j));
-    check[i][j] = true;
-    
-    
+
+// 상하좌우
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+// 정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸
+
+void BFS(int i, int j){
+
     while(!q.empty()){
-        
+    
         i = q.front().first;
         j = q.front().second;
         q.pop();
         
-        for(int z = 0; z<8; z++){
+        for(int z = 0; z<4; z++){
         
             int nx = i + dx[z];
             int ny = j + dy[z];
-        
-            if(nx >=0 && ny >= 0 && nx < h && ny < w ){
             
-                if(check[nx][ny] == false && arr[nx][ny] == 1){
-                    check[nx][ny] = true;
+            if(nx >= 0 && ny >= 0 && nx  < m && ny < n){
+            
+                if(check[nx][ny] == -1 && arr[nx][ny] == 0){
+                    
+                    check[nx][ny] = check[i][j] + 1;
                     q.push(make_pair(nx, ny));
+                    
                 }
-        
+            
             }
         }
-    }
+    } // while
+
 }
 
 
 
 int main(){
-    
-    int cnt = 0;
-    
-    while(1){
-        
-        cin >> w >> h;
-        cnt = 0;
-        
-        memset(arr, 0, sizeof(arr));
-        memset(check, false, sizeof(check));
-        
-        // 종료 구문
-        if( w == 0 && h == 0) break;
-        
-        for(int i=0; i<h; i++){
-            for(int j=0; j<w ; j++){
-                cin >> arr[i][j];
-            }
-        }
-        
-        for(int i=0; i<h; i++){
-            for(int j=0; j<w ; j++){
-                
-                if(check[i][j] == false && arr[i][j] == 1){
-                    BFS(i, j, ++cnt);
-                    
-                }
-            }
-        }
-        
-        cout << cnt <<'\n';
 
+    
+    // M은 상자의 가로 칸의 수, N 은 상자의 세로 칸의 수
+    cin >> n >> m;
+    
+    memset(check, -1, sizeof(check));
+    memset(arr, 0, sizeof(arr));
+    
+    for(int i=0 ; i<m; i++){
+        for(int j=0; j<n; j++){
+            cin >> arr[i][j];
+            
+            if(arr[i][j] == 1){
+                q.push(make_pair(i, j));
+                // 토마토 익는 날짜가 하루씩 낮은 부분을 해결하기 위해 미리 0으로 초기화
+                check[i][j] = 0;
+            }
+        }
     }
+    
+    
+    BFS(q.front().first, q.front().second);
+    
+    
+    // 토마토 익는 날짜 계산
+    // 체크한 수 중, 가장 큰 수를 최종 익는 날짜로 지정한다.
+    int ans = 0;
+    for(int i=0; i<m; i++){
+        for(int j=0;j<n; j++){
+            
+            if(ans < check[i][j]){
+                ans = check[i][j];
+            }
+        }
+    }
+    
+    for(int i=0 ; i<m; i++){
+        for(int j=0; j<n; j++){
+            // 단, 아직 익지 않은 토마토가 한개라도 있으면 -1
+            if( arr[i][j] == 0 && check[i][j] == -1){
+                ans = -1;
+            }
+        }
+    }
+
+    
+    cout << ans;
+
+
+
+
 
     return 0;
 }
-
-
-
