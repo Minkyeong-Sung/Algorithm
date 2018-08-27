a788bca4e45bf407aed90bd3614733fd59309220
Minkyeong <zoz7@naver.com>
2018-08-27 21:29:33 +0900

    2146 : 다리만들기(BFS*)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,79 +1,143 @@
 #include <iostream>
 #include <queue>
-#include <cstdio>
-#include <cstring>
 
-int n, m;
+
+using namespace std;
+
+int N;
 int arr[101][101];
+int land[101][101];
 int check[101][101];
 
 // 상하좌우
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
-using namespace std;
-
-
-// 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸
-
-void BFS(int sx, int sy, int fx, int fy){
-
-    queue<pair<int, int>> q;
-    
-    // 처음 시작 값 입력
-    q.push(make_pair(sx, sy));
-    check[sx][sy] = 1;
-    
-    while(!q.empty()){
-        
-        sx = q.front().first;
-        sy = q.front().second;
-        q.pop();
-        
-        for(int i=0; i<4; i++){
-        
-            int nx = sx + dx[i];
-            int ny = sy + dy[i];
-            
-            // 상하좌우 방향 탐색
-            if(nx >0 && ny >0 && nx <= n &&  ny <= m){
-                
-                // 아직 탐색하지 않았고, 갈 수 있는 길(1) 이라면 더 탐색
-                if(check[nx][ny] == -1 && arr[nx][ny] == 1){
-                    q.push(make_pair(nx, ny));
-                    check[nx][ny] = check[sx][sy] + 1;
-                }
-            }
-        }
-    } // while
-    
-    
-
-}
-
 
 int main(){
 
-    cin >> n >> m;
+    cin >> N;
+    int cnt = 0;
+
     
-    memset(check, -1, sizeof(check));
-    
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            scanf("%1d", &arr[i][j]);
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            
+            cin >> arr[i][j];
         }
     }
+    
+    // 섬의 개수 표시
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+        
+            if(land[i][j] == 0 && arr[i][j] == 1){
+                
+                queue<pair<int, int>> q;
+                
+                q.push(make_pair(i, j));
+                land[i][j] = ++cnt;
+                
+                while(!q.empty()){
+                    
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+                    
+                    for(int z = 0; z<4; z++){
+                        
+                        int nx = x + dx[z];
+                        int ny = y + dy[z];
+                        
+                        if(nx >= 0 &&  ny >= 0 && nx < N && ny < N){
+                            
+                            // 아직 방문 하지 않았다면,
+                            if(arr[nx][ny] == 1 && land[nx][ny] == 0 ){
+                                
+                                land[nx][ny] = cnt;
+                                q.push(make_pair(nx, ny));
+                                
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+    } // check land
+    
+    
+    queue<pair<int, int>> q;
+    
+    // 다른 섬까지의 거리를 기억하기 위해
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            
+            check[i][j] = -1;
+            
+            if(arr[i][j] == 1){
+                check[i][j] = 0;
+                q.push(make_pair(i, j));
+            }
+        }
+    }
+    
+    while(!q.empty()){
+    
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        
+        for(int i=0; i<4; i++){
+            
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            
+            if(nx >= 0 && ny >= 0 && nx < N && ny <N){
+                
+                // 주변에 바다가 있다면, 다리를 세울 거리 +1 씩
+                // 섬도 늘려감을 동시에 표시해줌
+                if(check[nx][ny] == -1){
+                
+                    check[nx][ny] = check[x][y] + 1;
+                    land[nx][ny] = land[x][y];
+                    q.push(make_pair(nx, ny));
+                    
+                }
+            }
+        } // 주변 탐색
+    } // while
+    
+    
+    // 초기값 설정
+    int ans = -1;
+    
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            for (int k=0; k<4; k++) {
+                
+                int nx = i + dx[k];
+                int ny = j + dy[k];
+                
+                if(nx >= 0 && ny >= 0 && nx < N && ny < N){
+                
+                    if(land[nx][ny] != land[i][j]){
+                        
+                        if (ans == -1 || ans > check[i][j] + check[nx][ny]) {
+                            ans = check[i][j] + check[nx][ny];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    cout << ans ;
+    
 
-    BFS(1, 1, n, m);
-    
-    
-    cout << check[n][m];
+
 
 
     return 0;
 }
-
-
-
-
-
