c8b906ff7f83e0e626d017abe8a063a809f5c337
Minkyeong <zoz7@naver.com>
2018-09-17 21:48:32 +0900

    [SWA]2819. 격자판의 숫자 이어 붙이기 - DFS

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,142 +1,80 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
-
+#include <cstring>
 
 using namespace std;
 
-int N;
-int arr[101][101];
-int land[101][101];
-int check[101][101];
+int dx[] = {-1 , 1, 0, 0};
+int dy[] = {0, 0 ,-1, 1};
+bool check[10000000];
+int arr[4][4];
+int result ;
 
-// 상하좌우
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
+void DFS(int x, int y ,string num){
 
+    num += to_string(arr[x][y]);
+    
+    // 재귀 종료 조건
+    if(num.size() == 7){
+        
+        int change = stoi(num);
+        
+        // 7자리 숫자가 완성되었을 떄 check 판단
+        if(check[change] == false){
+            result += 1;
+            check[change] = true;
+        }
+        
+        return;
+    }
+    
+    // 숫자 중복 사용가능
+    //check[x][y] = true;
+    
+    
+    for(int i=0; i< 4; i++){
+        
+        int tx = x + dx[i];
+        int ty = y + dy[i];
+        
+        if(tx >= 0 && ty >= 0 && tx < 4 && ty<4){
+            
+            DFS(tx, ty, num);
+            
+        }
+    }
+
+}
 
 int main(){
 
-    cin >> N;
-    int cnt = 0;
-
+    int testCase;
+    string num;
+    cin >> testCase;
     
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            
-            cin >> arr[i][j];
-        }
-    }
-    
-    // 섬의 개수 표시
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
+    for(int t=1; t<= testCase; t++){
         
-            if(land[i][j] == 0 && arr[i][j] == 1){
-                
-                queue<pair<int, int>> q;
-                
-                q.push(make_pair(i, j));
-                land[i][j] = ++cnt;
-                
-                while(!q.empty()){
-                    
-                    int x = q.front().first;
-                    int y = q.front().second;
-                    q.pop();
-                    
-                    for(int z = 0; z<4; z++){
-                        
-                        int nx = x + dx[z];
-                        int ny = y + dy[z];
-                        
-                        if(nx >= 0 &&  ny >= 0 && nx < N && ny < N){
-                            
-                            // 아직 방문 하지 않았다면,
-                            if(arr[nx][ny] == 1 && land[nx][ny] == 0 ){
-                                
-                                land[nx][ny] = cnt;
-                                q.push(make_pair(nx, ny));
-                                
-                            }
-                        }
-                    }
-                }
-                
-            }
-        }
-    } // check land
-    
-    
-    queue<pair<int, int>> q;
-    
-    // 다른 섬까지의 거리를 기억하기 위해
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            
-            check[i][j] = -1;
-            
-            if(arr[i][j] == 1){
-                check[i][j] = 0;
-                q.push(make_pair(i, j));
-            }
-        }
-    }
-    
-    while(!q.empty()){
-    
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
+        // 테스트 케이스마다 초기화 필요
+        memset(check, false, sizeof(check));
+        result = 0;
         
         for(int i=0; i<4; i++){
-            
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            
-            if(nx >= 0 && ny >= 0 && nx < N && ny <N){
-                
-                // 주변에 바다가 있다면, 다리를 세울 거리 +1 씩
-                // 섬도 늘려감을 동시에 표시해줌
-                if(check[nx][ny] == -1){
-                
-                    check[nx][ny] = check[x][y] + 1;
-                    land[nx][ny] = land[x][y];
-                    q.push(make_pair(nx, ny));
-                    
-                }
-            }
-        } // 주변 탐색
-    } // while
-    
-    
-    // 초기값 설정
-    int ans = -1;
-    
-    for (int i=0; i<N; i++) {
-        for (int j=0; j<N; j++) {
-            for (int k=0; k<4; k++) {
-                
-                int nx = i + dx[k];
-                int ny = j + dy[k];
-                
-                if(nx >= 0 && ny >= 0 && nx < N && ny < N){
-                
-                    if(land[nx][ny] != land[i][j]){
-                        
-                        if (ans == -1 || ans > check[i][j] + check[nx][ny]) {
-                            ans = check[i][j] + check[nx][ny];
-                        }
-                    }
-                }
+            for(int j=0; j<4; j++){
+                cin >> arr[i][j];
             }
         }
+        
+        // DFS 탐색 : 끝(7자리수)이 정해져 있어서 사용
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                DFS(i, j, "");
+            }
+        }
+        
+        cout << "#" << t << ' ' << result <<'\n';
     }
     
-    cout << ans ;
-    
-
-
 
 
     return 0;
