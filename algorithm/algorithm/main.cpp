b87e34ce1a1093a1e99596b0d9138939944e379b
Minkyeong <zoz7@naver.com>
2018-09-30 14:57:02 +0900

    1759 암호만들기 (재귀)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,96 +1,70 @@
 #include <iostream>
-#include <map>
 #include <vector>
-#include <string>
+#include <cstring>
+
 
 using namespace std;
 
 
-vector<pair<int,int>> vt[1001][1001];
-bool check[1001][1001];
+// 만들 암호 길이 , 현재까지 만든 암호 , 사용가능한 알파벳, 사용할지말지 결정해야하는 인덱스
+void makePassword(int n, string password, vector<char> &vt, int index){
 
-
-void checkRoad(int x, int y, int toX, int toY){
-
-    for(int i=x; i<= toX; i++){
-        for(int j=y; j<= toY; j++){
-            
-            if((i== x || i == toX) && j < toY){
-                
-                vt[i][j].push_back(make_pair(i, j+1));
-                vt[i][j+1].push_back(make_pair(i, j));
+    if(password.length() == n){
+        
+        int jaum=0, moum=0;
+        
+        for(char x : password){
+        
+            if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'){
+                moum += 1;
             }
-            
-            if((j==y || j== toY) && i < toX){
-                
-                vt[i][j].push_back(make_pair(i+1, j));
-                vt[i+1][j].push_back(make_pair(i, j));
+            else{
+                jaum += 1;
             }
-            
+        }
+        
+        // 암호 생성 조건 만족
+        if(moum >= 1  && jaum >= 2){
+        
+            cout << password <<'\n';
+            return;
         }
     }
-}
-
-void DFS(int x, int y){
-
     
-    check[x][y] = true;
-    
-    for(int i=0; i< vt[x][y].size(); i++){
-        
-        int nx = vt[x][y][i].first;
-        int ny = vt[x][y][i].second;
-        
-        if(check[nx][ny] == false){
-            DFS(nx, ny);
-        }
+    // 배열 크기를 넘어가는 경우 (기저사례)
+    if(index >= vt.size()){
+        return;
     }
+    
 
-    return;
+    // index번째 알파벳을 사용하는 경우
+    makePassword(n, password + vt[index], vt, index+1);
+    // 사용하지 않는 경우
+    makePassword(n, password, vt, index+1);
+    
+
 
 }
-
 
 int main(){
     
-    int n, cnt = 0;
-    int x1, x2, y1, y2;
-    int sx = 500, sy = 500; // 음수를 없애기 위해
+    vector<char> vt;
+    int L, C;
+    char ch;
+    // L개의 알파벳 생성
+    cin >> L >> C;
     
-    cin >> n;
-    
-    for(int i=0; i<n; i++){
+    for(int i=0; i<C; i++){
+        cin >> ch;
         
-        cin >> x1 >> y1 >> x2 >> y2;
-        
-        checkRoad(x1+500, y1+500, x2+500, y2+500);
+        vt.push_back(ch);
     }
     
-    
-    
-    for(int i=0; i<=1000; i++){
-        for(int j=0; j<=1000; j++){
-        
-            if(check[i][j] == false && vt[i][j].size() != 0){
-            
-                DFS(i, j);
-                cnt++;
-                
-            }
-            
-        }
-    }
-    
-    // 거북이의 출발점에서부터 줄을 그을 수 있는 경우
-    if( vt[sx][sy].size() != 0){
-        cnt -= 1;
-    
-    }
-    
-    cout << cnt;
-    
-    
-    
+    sort(vt.begin(), vt.end());
+
+
+    makePassword(L,"", vt, 0);
+
 
     return 0;
 }
