2025bfc7af4658fa8bf3e174e74065639a8e87f9
Minkyeong <zoz7@naver.com>
2018-10-01 00:06:17 +0900

    6603 로또 : 조합(뽑는경우, 뽑지 않는 경우)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,59 +1,49 @@
 #include <iostream>
-
+#include <vector>
 
 using namespace std;
 
-char arr[21][21];
-bool alpha[26];
-int r, c, maxNum=1;
+int arr[50];
+int ans[50];
+int N;
 
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
+void lotto(int s, int n){
 
-
-void DFS(int x, int y, int cnt){
-    
-    if(maxNum < cnt){
-        maxNum = cnt;
-    }
-    
-    for(int i=0; i<4; i++){
-    
-        int nx = x + dx[i];
-        int ny = y + dy[i];
+    if(n == 6){
         
-        if(nx >=0 && ny >= 0 && nx < r && ny < c && alpha[arr[nx][ny] - 65] == false ){
-        
-            alpha[arr[nx][ny] - 65] = true;
-            
-            DFS(nx, ny, cnt+1);
-            
-            alpha[arr[nx][ny] - 65] = false;
-        
+        for(int i=0; i<6; i++){
+            cout << ans[i] << ' ';
         }
+        cout <<'\n';
+        return;
     }
+    
+    for(int i=s; i<N; i++){
+    
+        ans[n] = arr[i];
+        lotto(i+1, n+1);
+    }
+    
+
 }
-
-
 
 int main(){
     
-    cin >> r >> c;
     
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
-            cin >> arr[i][j];
+    while(1){
+        
+        cin >> N;
+        
+        if(N==0) break;
+        
+        for(int i=0; i<N; i++){
+            cin >> arr[i];
         }
+    
+        lotto(0, 0);
+        cout <<'\n';
+    
     }
-    
-    alpha[arr[0][0] - 65] = true;
-    
-    DFS(0,0, 1);
-    
-    
-    cout << maxNum;
-
-
 
     return 0;
 }
