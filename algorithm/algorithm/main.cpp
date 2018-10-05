5e0323323bba09d69f733ed38744d62a130824ec
Minkyeong <zoz7@naver.com>
2018-10-05 12:31:16 +0900

    1952. [모의 SW 역량테스트] 수영장 : 백트래킹/DFS

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,118 +1,80 @@
 #include <iostream>
 #include <cstring>
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
 
-int N;
-int arr[22][22];
-bool number[101];
+int u1, u2, u3, u4;
+int month[13];
+bool check[13];
+int ans;
 
-// 오른쪽 아래 - 왼쪽 아래 - 왼쪽 위 - 오른쪽 위
-int dx[] = {1, 1, -1, -1};
-int dy[] = {1, -1, -1, 1};
-int ans ,maxCnt;
-
-
-void DFS(int x, int y, int sx, int sy, int dir ){
+// 백트래킹
+void solve(int cnt , int sum){
     
-    // 방향 도착
-    if(dir == 4){
+    // 12 이상이 되면 무조건 최소값을 구하고 종료시키면 된다!
+    // 12이상일때 i 를 다시 1월달로 옮기면 문제발생! ** 주의 할 것
     
-        if(x == sx && y == sy){
-           
-            ans = 0;
-            for(int i=0; i<101; i++){
-                
-                if(number[i]== true){
-                    ans += 1;
-                }
-            }
-            maxCnt = max(maxCnt, ans);
-            
-            return;
-            
-        }
+    if(cnt >= 12){
+        ans = min(ans, sum);
+        return;
     }
     
     
-    int nx = x + dx[dir];
-    int ny = y + dy[dir];
+    if(month[cnt] > 0 && check[cnt] == false){
     
-    // 범위 초과
-    if(nx <0 || ny < 0 || nx >= N || ny >= N) return;
+        check[cnt] = true;
+        
+        //1일권
+        solve(cnt+1, sum + (u1 * month[cnt]));
+        
+        //1달권
+        solve(cnt+1, sum + u2);
+        
+        //3달권
+        solve(cnt+3, sum + u3);
+    
+    
+    }
     else{
-    
-        // 처음 방문하는 디저트 카페 집이라면
-        if(number[arr[nx][ny]] == false){
-            
-            number[arr[nx][ny]] = true;
-            
-            // 이 방향 그대로 하는 경우
-            DFS(nx, ny, sx, sy, dir);
-            
-            // 방향을 바꿔서 탐색해보는 경우
-            DFS(nx, ny, sx, sy, dir+1);
-            
-            /* 백 트 래 킹 */
-            number[arr[nx][ny]] = false;
-            
-            
-        }
-        else{
-            // 방문 했지만, 다음 방향으로 돌려서 탐색하는 경우
-            /* 다른 경우로 재 탐색 하는 경우, "처음" 좌표에서 가는 것   */
-            DFS(x, y, sx, sy, dir+1);
-        }
-
+        
+        solve(cnt+1, sum);
     }
+    
+    
+    check[cnt] = false;
 }
-
 
 
 int main(){
-
+    
     int testCase;
     
     cin >> testCase;
-    
-    for(int t=1; t<= testCase; t++){
-    
-        cin >> N;
-        
-        
-        
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N ; j++){
-                cin>> arr[i][j];
-                
-            }
-        }
-        
-        // 백트래킹
-        for(int i=0; i<N-1; i++){
-            for(int j=1; j<N-1; j++){
-                
-                number[arr[i][j]] = true;
-                DFS(i, j, i+1, j-1, 0);
-                number[arr[i][j]] = false;
-            
-            }
-        }
 
-        if(maxCnt == 0) maxCnt = -1;
+    for(int  t= 1; t<= testCase; t++){
         
-        cout << "#" << t << ' ' << maxCnt <<'\n';
+        cin >> u1 >> u2 >> u3 >> u4;
         
-        ans = 0;
-        maxCnt = 0;
-        memset(number, false, sizeof(number));
+        // init
+        // 1년권을 사용하는 것을 잡아두기
+        ans = u4;
+        
+        memset(check, false, sizeof(check));
+        
+        for(int i=1; i<=12; i++){
+            cin >> month[i];
+        }
+        
+        solve(1, 0);
+        
+        //가장 적은 비용으로 수영장을 이용할 수 있는 방법을 찾고 그 비용을 정답으로 출력
+        cout << '#' << t << ' ' << ans <<'\n';
         
     }
 
-
-
-
-
     return 0;
 }
+
+
