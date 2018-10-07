5bcec08e049edaa28311ec2fc3d40a21c54ccb63
Minkyeong <zoz7@naver.com>
2018-10-07 19:18:59 +0900

    5656. [모의 SW 역량테스트] 벽돌 깨기 : 배열 값 깊이만큼 탐색하는 BFS ** dx[] dy[] 따지기 전에 깊이만큼 반복문 쓸것 **

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,63 +1,169 @@
 #include <iostream>
+#include <cstring>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int arr[10][10];
+int N, W, H;
+int ans = 987654321;
+int arr[15][12];
 
-void printN(int n){
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+struct Node{
+
+    int x;
+    int y;
+    int depth;
+};
+
+void breakBlock(int x, int y, int depth){
+
+    queue<Node> q;
+    q.push({x, y, depth});
+    arr[x][y] = 0;
     
-    int N = n;
-    int dir = 1;
-    int x = 0;
-    int y = -1;
-    int num = 1;
+    while(!q.empty()){
+        
+        int tx = q.front().x;
+        int ty = q.front().y;
+        int td = q.front().depth;
+        q.pop();
+        
+        /*  여기서 깊이만큼 한번에 탐색 가능! */
+        for(int i=1; i<td; i++){
+            for(int d=0; d<4; d++){
+                
+                // 배열에 저장된 깊이만큼 바로 이동한지 탐색!
+                int nx = tx + dx[d]*i;
+                int ny = ty + dy[d]*i;
+                int nd = arr[nx][ny];
+                
+                if(nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+                
+                if(arr[nx][ny] !=0){
+                    q.push({nx, ny, nd});
+                    arr[nx][ny] = 0;
+                }
+            }
+        
+        }
+    }
+}
+
+void moveBlock(){
+
+    for(int i=0; i<W; i++){
     
-    while (1) {
+        for(int j= H-2; j>= 0; j--){
         
-        // 가로
-        for(int i=0; i<n; i++){
-            y = y + dir;
-            arr[x][y] = num++;
+            int x = j;
+            int y = i;
+            
+            if(arr[x][y] == 0) continue;
+            
+            while(1){
+                
+                // 아래서 부터 거꾸로 비교!
+                if(arr[x+1][y] == 0 && x+1 >= 0 && y >= 0 && x+1 < H && y <= W){
+                 
+                    arr[x+1][y] = arr[x][y];
+                    arr[x][y] = 0;
+                    x++;
+                }else{
+                    break;
+                }
+            }
         }
-        
-        // 횟수 줄임
-        n--;
-        
-        if(n<0 ) break;
-        
-        // 세로
-        for(int i=0; i<n; i++){
-            x = x + dir;
-            arr[x][y] = num++;
+    }
+}
+
+void solve(int k){
+    
+    int tmp[15][12];
+    
+    // 백트래킹!
+    for(int j=0; j<H; j++){
+        for(int l = 0; l<W; l++){
+            tmp[j][l] = arr[j][l];
         }
-        
-        dir *= -1;
     }
     
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            cout << arr[i][j] <<  ' ';
+    
+    if(k == N){
+    
+        int cnt = 0;
+        
+        for(int i=0; i< H; i++){
+            for(int j=0; j<W; j++){
+                
+                if(arr[i][j] != 0){
+                    cnt += 1;
+                }
+            }
         }
-        cout <<'\n';
+        
+        ans = min(ans, cnt);
+    
+        return;
     }
-
+    
+    
+    // 구술 차는 경우의 수
+    for(int i=0; i<W; i++){
+        
+        int x = 0;
+        int y = i;
+        
+        // 제일 위에 있는 구술 찾기
+        while(x>= 0 && y>= 0 && x < H && y < W && arr[x][y] == 0){
+            x++;
+        }
+        
+        // 구술 치기 시작!
+        breakBlock(x, y, arr[x][y]);
+    
+        moveBlock();
+        
+        solve(k+1);
+        
+        // 백트래킹!
+        for(int j=0; j<H; j++){
+            for(int l = 0; l<W; l++){
+                arr[j][l] = tmp[j][l];
+            }
+        }
+        
+    }
+    
+    
 
 }
 
 int main(){
-
-    int tc, N;
+    
+    int tc;
     
     cin >> tc;
     
     for(int t=1; t<= tc; t++){
+        cin >> N >> W >> H;
         
-        cin >>N;
-    
-        cout << "#"<< t << '\n';;
-        printN(N);
-    
+        ans = 987654321;
+        
+        for(int i=0; i<H; i++){
+            for(int j=0; j<W; j++){
+                cin >> arr[i][j];
+            }
+        }
+        
+        solve(0);
+        
+        cout << "#" << t << ' ' << ans <<'\n';
     }
+
 
 
     return 0;
