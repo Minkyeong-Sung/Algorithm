ed06d96e3fb737817d51254818a86304a2693092
Minkyeong <zoz7@naver.com>
2018-10-09 18:04:51 +0900

    2382. [모의 SW 역량테스트] 미생물 격리 : 이동 경로 + 위치 중복 확인하기 ( 이차원 벡터를 이용하여 저장)

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,84 +1,146 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int N, K;
-int digit[29];
-vector<int> vt;
+int N, M, K;
 
-void rotation(){
+typedef struct {
+    int x;
+    int y;
+    int n;
+    int dir;
+
+}Node;
+
+int dx[] = {0, -1, 1, 0, 0};
+int dy[] = {0, 0, 0, -1, 1};
+
+vector<int> map[100][100]; // 충돌나는 미생물을 저장하기 위해
+vector<Node> bug(1005); // 미생물 저장
+
+int reversDir(int dir){
     
-    int tmp = digit[N-1];
-    
-    for(int i= N-1; i>0 ; i--){
-        digit[i] = digit[i-1];
-    }
-    digit[0] = tmp;
+    if(dir == 1) return 2;
+    else if(dir == 2) return 1;
+    else if(dir == 3) return 4;
+    else return 3;
 }
 
-void calculate(){
-    
-    for(int i=0; i<N; i+= N/4){
-        int tmp = 0;
-        int l = 0;
+
+void solve(){
+    // 격리시간만큼만
+    for(int t=0; t<M; t++){
         
-        for(int j=i; j< i+ N/4; j++){
-            tmp += digit[j] * pow(16, (N/4) -1 - l);
-            l++;
+        // init
+        for(int i=0; i<K; i++){
+            // 미생물이 없다면 그냥 넘어감
+            if(bug[i].n == 0){
+                continue;
+            }
+            map[bug[i].x][bug[i].y].clear();
         }
         
-        vt.push_back(tmp);
+        // 미생물 이동
+        for(int i=0; i<K; i++){
+            
+            if(bug[i].n == 0) continue;
+            // 이동
+            bug[i].x += dx[bug[i].dir];
+            bug[i].y += dy[bug[i].dir];
+            map[bug[i].x][bug[i].y].push_back(i); // i번째 미생물 넣어줌
+        }
+        
+        // 이동시킨 미생물이 충돌하는지, 가장자리에 있는지 확인하여 정렬
+        for(int i=0; i<K; i++){
+            
+            if(bug[i].n == 0) continue;
+            
+            // 가장자리에 있는 경우
+            if(bug[i].x == 0 || bug[i].y == 0 || bug[i].x == N-1 || bug[i].y == N-1){
+                /* 미생물 수 절반, 반대 방향  */
+                bug[i].n /= 2;
+                bug[i].dir = reversDir(bug[i].dir);
+                
+            }
+            // 충돌 나는 경우
+            else if(map[bug[i].x][bug[i].y].size() > 1){
+                /* 두개 이상인 경우면 충돌 */
+                int x = bug[i].x;
+                int y = bug[i].y;
+                
+                int tmpMax = 0;
+                int tmpDir = 0;
+                int tmpSum =0;
+                int tmpIndex = 0;
+                // 해당 좌표에 존재하는 미생물 수만큼 충돌 해결한다!
+                for(int j=0; j< map[x][y].size() ; j++){
+                    
+                    tmpSum += bug[map[x][y][j]].n;
+                    
+                    // 해당 좌표에 존재하는 미생물들 중, j번째 미생물과 비교
+                    if(tmpMax <  bug[map[x][y][j]].n){
+                        tmpMax = bug[map[x][y][j]].n;
+                        tmpDir = bug[map[x][y][j]].dir;
+                        tmpIndex = map[x][y][j];
+                    }
+                    
+                    // 합친 후, 해당 값은 0으로 !
+                    bug[map[x][y][j]].n = 0;
+                    
+                }
+                // 충돌 합친 후, 새로운 값으로 갱신
+                bug[tmpIndex].n = tmpSum;
+                bug[tmpIndex].dir = tmpDir;
+                
+            }
+            
+        }
+        
     }
-    
 }
 
 int main(){
     
     int tc;
-    string input;
-    
+
     cin >> tc;
     
     for(int t=1; t<= tc; t++){
-        cin >> N >> K;
-        cin >> input;
         
-        // 숫자로 변환
-        for(int i=0; i<N; i++){
-            
-            if(input[i] <'A'){
-                digit[i] = input[i] - '0';
-            }
-            else{
-                digit[i] = input[i] - 'A' + 10;
-            }
-        }
+        cin >> N >> M >> K;
         
-        // 한 변의 길이만큼 회전
-        for(int i=0; i<N/4; i++){
-            rotation();
-            calculate();
-        }
-        
-        // 내림차순 정렬
-        sort(vt.begin(), vt.end(), greater<int>());
-        
-        /* 중복되는 수가 있으면, 그 해당 수만큼 늘리기
-         단, 전체 반복문은 찾는 인덱스까지로 설정하고 돌릴 것!!
-         왜냐면, 그 이후의 수들까지 카운트를 해버리면 오류 발생 ** */
         for(int i=0; i<K; i++){
+            cin >> bug[i].x >> bug[i].y >> bug[i].n >> bug[i].dir;
+            map[bug[i].x][bug[i].y].push_back(i);
+        }
+        
+        solve();
+        
+        int ans = 0;
+        for(int i=0;i<K; i++){
             
-            if(i >= 0 && vt[i] == vt[i-1]){
-                K++;
+            ans += bug[i].n;
+        }
+        
+        // 초기화
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                map[i][j].clear();
             }
         }
         
-        cout << "#" << t << ' ' << vt[K-1] <<'\n';
         
-        vt.clear();
+        cout << "#" << t << ' ' <<ans <<'\n';
+        
     }
+    
+    
+    
+    
+    
     return 0;
 }
+
+
+
