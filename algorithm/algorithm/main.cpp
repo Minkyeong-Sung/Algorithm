7a9b942a7669511fcae20b1820fed1282f3a6439
Minkyeong <zoz7@naver.com>
2018-10-12 23:39:09 +0900

    2112. [모의 SW 역량테스트] 보호 필름 : 백트래킹/재귀

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,115 +1,86 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-int N, M, C;
-int arr[11][11];
-int maxCal = 0;
-int ans = 0;
+int D, W, K;
 
-void selectBox(vector<int> box1, int sum, int mul, int idx){
+int arr[13][20];
+int ans = 987654321;
+
+bool checkFilm(){
     
-    if(idx >= box1.size()){
+    for(int i=0; i<W; i++){ // 열
+
+        int check = 0;
         
-        if(sum <= C){
-            maxCal = max(maxCal , mul);
-        }
-        return;
-    }
-    
-    selectBox(box1, sum ,mul, idx+1);
-    selectBox(box1, sum + box1[idx], mul + ( box1[idx] * box1[idx]), idx+1);
-}
-
-void checkRangeOfHoney(vector<int> box1, vector<int> box2){
-    
-    int sum1 = 0, sum2 = 0;
-    int ans1 = 0, ans2 = 0;
-    int tmpAns=0;
-    
-    for(auto q: box1){
-        sum1 += q;
-        ans1 += (q*q);
-    }
-    
-    
-    if(sum1 > C){
-        maxCal=0;
-        selectBox(box1, 0,0, 0);
-        tmpAns += maxCal;
-    }
-    else{
-        tmpAns += ans1;
-    }
-    
-    for(auto q : box2){
-        sum2 += q;
-        ans2 += (q*q);
-    }
-    
-    if(sum2 > C){
-        maxCal = 0;
-        selectBox(box2, 0, 0,0);
-        tmpAns += maxCal;
-    }
-    else{
-        tmpAns += ans2;
-    }
-    
-    ans = max(ans, tmpAns);
-    
-    return;
-}
-
-
-void combiBox2(vector<int> box1, vector<int> box2, int x, int y ){
-    
-    if(box2.size() == M){
-        
-        // 벌꿀 채취 시작
-        checkRangeOfHoney(box1, box2);
-        return;
-    }
-    
-    if(y < N) {
-        
-        box2.push_back(arr[x][y]);
-        combiBox2(box1, box2, x, y+1);
-    }
-    
-    
-}
-
-void combiBox(vector<int> box1, int x, int y ){
-    
-    // 벌꿀 1을 뽑은 후, 2를 뽑자
-    if(box1.size() == M){
-        
-        if(y >= N){
-            y-=1;
-        }
-        vector<int> box2;
-        bool flag = false;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                
-                if(i == x && j == y) flag = true;
-                
-                if(flag == true){
-                    combiBox2(box1, box2, i, j);
-                }
+        for(int j=1; j<D; j++){ // 행
+            
+            if(arr[j][i] == arr[j-1][i]){
+                check += 1;
+            }
+            else{
+                check = 0;
+            }
+            
+            // 만족하므로 더 탐색할 필요없다
+            if(check >= K-1){
+                break;
             }
         }
+        
+        if(check < K-1 ){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+void solve(int depth, int cnt){
+    
+    // 현재 답보다 더 큰 경우라면, 더 탐색할 필요가 없음
+    if(cnt >= ans){
         return;
     }
     
-    if(y <N){
-        
-        box1.push_back(arr[x][y]);
-        combiBox(box1, x, y+1);
+    // 종료조건1 : 마지막 행까지 본경우 종료
+    if(depth == D ){
+    
+        if(checkFilm()== true){
+            ans = min(cnt, ans);
+        }
+        return;
+    }
+    
+    int copyArr[20] = {0};
+    
+    // 아무것도 하지 않는 경우
+    solve(depth+1, cnt);
+    
+    // 0 투입하는 경우
+    // 초기 상태 저장
+    // 그 후, 해당 행을 약품 투입한 것으로 변경
+    for(int i=0; i<W; i++){
+        copyArr[i] = arr[depth][i];
+        arr[depth][i] = 0;
+    }
+    
+    solve(depth+1, cnt+1);
+    
+    // 1 투입하는 경우
+    // 그 후, 해당 행을 약품 투입한 것으로 변경
+    for(int i=0; i<W; i++){
+        arr[depth][i] = 1;
+    }
+    solve(depth+1, cnt+1);
+    
+    // 백트래킹
+    // 원상 복구
+    for(int i=0; i<W; i++){
+        arr[depth][i] = copyArr[i];
     }
     
 }
@@ -120,26 +91,19 @@ int main(){
     
     for(int t=1; t<= tc; t++){
         
-        cin >> N >> M >> C;
-        ans = 0;
+        cin >> D >> W >> K;
         
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                cin >>arr[i][j];
+        for(int i=0; i<D; i++){
+            for(int j=0; j<W; j++){
+                cin >> arr[i][j];
             }
         }
         
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                vector<int> box1;
-                combiBox(box1, i, j);
-            }
-        }
+        ans = 987654321;
+        solve(0, 0);
         
-        cout <<"#" << t << ' '<< ans <<'\n';
+        cout << "#" << t << ' ' << ans <<'\n';
     }
-    
-    
     
     return 0;
 }
