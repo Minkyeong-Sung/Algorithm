35b1fdec22bebfe6a574049779e9ffa5e6f724de
Minkyeong <zoz7@naver.com>
2018-10-14 19:22:49 +0900

    [모의 SW 역량테스트] 홈 방범 서비스: 깊이를 알 수 없는 BFS

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,104 +1,99 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 #include <cstring>
+#include <queue>
 
 using namespace std;
+
+int N, M;
+int ans = 0;
+int arr[20][20];
+bool check[20][20];
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
 
 typedef struct{
     int x;
     int y;
-    int d;
-    int e;
+    int depth;
+}Node;
+
+void BFS(int x, int y){
     
-} Node;
-
-int arr[4001][4001];
-int check[4001][4001];
-vector<Node> ball;
-
-int dx[] = { 0 , 0, -1, 1};
-int dy[] = {1, -1, 0, 0};
-int N, ans = 0;
-
-void solve(){
+    queue<Node> q;
+    q.push({x, y, 1});
+    check[x][y] = true;
+    int house = arr[x][y];
     
-    // 1개만 남았을 때는 더이상 변화 없다
-    while( ball.size() >2){
+    /*  BFS를 깊이 몇까지 실행할지 알 수 없을때 사용하는 방법  */
+    /*
+        현재 탐색하는 깊이 D= 1로 지정한다.
+        큐에 저장된 깊이와 현재 탐색하는 깊이가 같으면 BFS 수행 완료
+        그 후, 크기를 한개씩 증가해가며 계속 탐색한다
+     */
+    int D = 1;
+    
+    while(!q.empty()){
         
-        // 움직이기
-        for(int i=0; i< ball.size(); ){
-            
-            // 원래 있던 자리는 아웃
-            check[ball[i].y][ball[i].x] -=1;
-            
-            ball[i].x += dx[ball[i].d];
-            ball[i].y += dy[ball[i].d];
-            
-            // overflow
-            // 초과하면 더이상 고려하지 않는다
-            if(ball[i].x < 0 || ball[i].y < 0 ||  ball[i].x >= 4000 || ball[i].y >= 4000){
-                ball.erase(ball.begin()+ i);
-               // i--;
+        int tx = q.front().x;
+        int ty = q.front().y;
+        int td = q.front().depth;
+        q.pop();
+        
+        /*  여기가 다음 깊이로 가는 방법 */
+        if(td == D){
+            if((M*house) - ((D*D) + (D-1)*(D-1)) >= 0){
+                ans = max(ans, house);
             }
-            else{
-                check[ball[i].y][ball[i].x] += 1;
-                i+= 1;
-            }
+            D++;
         }
         
-        // 충돌위치 확인
-        vector<pair<int, int>> vt;
-        for(int i=0; i<ball.size(); ){
+        for(int d=0; d<4; d++){
             
-            if(check[ball[i].y][ball[i].x] >=2){
+            int nx = tx + dx[d];
+            int ny = ty + dy[d];
             
-                ans += ball[i].e;
-                // 충돌 처리 완료 한 후
-                vt.push_back({ball[i].x, ball[i].y});
-                ball.erase(ball.begin()+i);
-               // i--;
-            }
-            else{
-                i++;
+            if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+            if(check[nx][ny] == true) continue;
+            
+            q.push({nx, ny, td+1});
+            check[nx][ny] = true;
+            
+            if(arr[nx][ny] == 1){
+                house++;
             }
         }
-       
-        for(int i=0; i< vt.size(); i++){
-            check[vt[i].second][vt[i].first] = 0;
-        }
-        
     }
+    
+    return;
 }
 
 int main(){
     
     int tc;
-    int x, y, d, e;
     cin >> tc;
     
-    for (int t=1; t<= tc; t++) {
-        cin >> N;
-        
-        
+    for(int t=1; t<= tc; t++){
+        cin >> N >> M;
         ans = 0;
-        memset(check,0,sizeof(check));
         
         for(int i=0; i<N; i++){
-            cin >> x >> y >> d >> e;
-            
-            x = (x+1000)*2;
-            y = (y+1000)*2;
-            
-            ball.push_back({x, y, d, e});
-            check[y][x] = 1;
+            for(int j=0; j<N; j++){
+                cin >>arr[i][j];
+            }
         }
-    
-        solve();
-       
         
-        cout <<"#" << t << ' ' << ans <<'\n';
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                memset(check, false, sizeof(check));
+                BFS(i, j);
+            }
+        }
+        
+        cout << "#" << t << ' ' << ans <<"\n";
     }
-    
-    
+
     return 0;
 }
