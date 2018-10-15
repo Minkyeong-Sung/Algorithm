33e965dcb0866b2eccc19f886662d6752562cb4a
Minkyeong <zoz7@naver.com>
2018-10-15 14:40:19 +0900

    5653. [모의 SW 역량테스트] 줄기세포배양 : 문제따라 조건 설정하기 / 배열 탐색

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -5,95 +5,118 @@
 
 using namespace std;
 
-int N, M;
-int ans = 0;
-int arr[20][20];
-bool check[20][20];
+int N, M, K;
 
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
-
 typedef struct{
-    int x;
-    int y;
-    int depth;
+    int data;
+    int start_t;
+    int end_t;
+    int state; // -1(죽음) 0(아무것도 없음) 1(비활성) 2(활성)
+    int input_t;
 }Node;
 
-void BFS(int x, int y){
+int ans = 0;
+Node arr[352][352];
+
+void solve(){
     
-    queue<Node> q;
-    q.push({x, y, 1});
-    check[x][y] = true;
-    int house = arr[x][y];
-    
-    /*  BFS를 깊이 몇까지 실행할지 알 수 없을때 사용하는 방법  */
-    /*
-        현재 탐색하는 깊이 D= 1로 지정한다.
-        큐에 저장된 깊이와 현재 탐색하는 깊이가 같으면 BFS 수행 완료
-        그 후, 크기를 한개씩 증가해가며 계속 탐색한다
-     */
-    int D = 1;
-    
-    while(!q.empty()){
+    // 주어진 시간동안만 진행한다
+    for(int T=1; T<=K; T++){
         
-        int tx = q.front().x;
-        int ty = q.front().y;
-        int td = q.front().depth;
-        q.pop();
-        
-        /*  여기가 다음 깊이로 가는 방법 */
-        if(td == D){
-            if((M*house) - ((D*D) + (D-1)*(D-1)) >= 0){
-                ans = max(ans, house);
+        for(int i=0 ; i < K + N; i++){
+            for(int j= 0 ; j <K + M; j++){
+                
+                if(arr[i][j].data == 0) continue;
+                
+                // 활성화 상태도 바꾸기
+                if(arr[i][j].start_t == T){
+                    arr[i][j].state = 2;
+                }
+                
+                // t초 시간 후 활성
+                if(arr[i][j].state == 2 && arr[i][j].start_t == T-1){
+                    
+                    for(int d=0; d<4; d++){
+                        
+                        int nx = i + dx[d];
+                        int ny = j + dy[d];
+                        
+                        // 원래 빈 공간으로 확장하거나
+                        // t초가 지났을때 증식하는 경우
+                        if(arr[nx][ny].state == 0 || (arr[nx][ny].state == 1 && arr[nx][ny].input_t == T && arr[nx][ny].data < arr[i][j].data)){
+                            
+                            arr[nx][ny].start_t = arr[i][j].data + T;
+                            arr[nx][ny].end_t = arr[i][j].data * 2 + T;
+                            arr[nx][ny].data = arr[i][j].data;
+                            arr[nx][ny].state = 1;
+                            arr[nx][ny].input_t = T;
+                        }
+                    }
+                }
+                
+                // 죽은 시간일때
+                if(arr[i][j].state == 2 && arr[i][j].end_t == T){
+                    arr[i][j].state = -1;
+                }
             }
-            D++;
         }
-        
-        for(int d=0; d<4; d++){
-            
-            int nx = tx + dx[d];
-            int ny = ty + dy[d];
-            
-            if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-            if(check[nx][ny] == true) continue;
-            
-            q.push({nx, ny, td+1});
-            check[nx][ny] = true;
-            
-            if(arr[nx][ny] == 1){
-                house++;
-            }
-        }
+        // t초 경과
+        /*
+         cout << T << "초" << '\n';
+         for(int i=0 ; i < K + N; i++){
+         for(int j= 0 ; j <K + M; j++){
+         cout << arr[i][j].data << ' ';
+         }
+         cout <<'\n';
+         }*/
     }
-    
-    return;
 }
+
 
 int main(){
     
-    int tc;
+    int tc, input;
     cin >> tc;
-    
     for(int t=1; t<= tc; t++){
-        cin >> N >> M;
+        cin >> N >> M >> K;
+        
+        // init
         ans = 0;
         
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                cin >>arr[i][j];
+        for(int i= K/2;  i< K/2 + N; i++){
+            for(int j= K/2; j <K/2 + M; j++){
+                cin >> input;
+                
+                if(input != 0){
+                    arr[i][j].data = input;
+                    arr[i][j].start_t = input;
+                    arr[i][j].end_t = 2*input;
+                    arr[i][j].state = 1;
+                }
             }
         }
         
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                memset(check, false, sizeof(check));
-                BFS(i, j);
+        solve();
+        
+        for(int i=0; i< N+K; i++){
+            for(int j=0; j<M+K; j++){
+                
+                if(arr[i][j].state == 1 || arr[i][j].state == 2){
+                    ans+=1;
+                }
+                arr[i][j].data =0;
+                arr[i][j].end_t = 0;
+                arr[i][j].state = 0;
+                arr[i][j].start_t = 0;
+                arr[i][j].input_t = 0;
             }
         }
         
-        cout << "#" << t << ' ' << ans <<"\n";
+        cout <<"#" << t << ' ' << ans << "\n";
+        
     }
-
     return 0;
 }
