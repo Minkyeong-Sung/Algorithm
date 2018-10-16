7238006216ebcabcc27380706ee900c42cd697ef
Minkyeong <zoz7@naver.com>
2018-10-16 15:46:57 +0900

    15683 감시 : 백트레킹 / 완전탐색

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,94 +1,237 @@
 #include <iostream>
-#include <queue>
-#include <utility>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int N, K, L;
-int arr[102][102];
-vector<pair<int, int>> snake;
-queue<pair<int, char>> q;
+int N, M;
+int ans = 987654321;
+vector<pair<int, int>> vt;
 
-int dir = 1;
-
-// 상 우 하 좌
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
-
-void changeDir(char d){
+void upArr(int (*cpyArr)[8], int x, int y, int n){
     
-    if(d == 'L'){
-        dir = (dir + 3)%4;
-    }
-    else{
-        dir = (dir + 1 ) %4;
+    for(int i=x-1; i >=0; i--){
+        if(cpyArr[i][y] == 6) break;
+        if(cpyArr[i][y] >= 1 && cpyArr[i][y] <= 5) continue;
+        cpyArr[i][y] = n;
+    
     }
 }
 
-void findsnakeTime(){
+void downArr(int (*cpyArr)[8], int x, int y, int n){
     
-    int t = 0;
+    for(int i=x+1; i < N; i++){
+        if(cpyArr[i][y] == 6) break;
+        if(cpyArr[i][y] >= 1 && cpyArr[i][y] <= 5) continue;
+        cpyArr[i][y] = n;
+
+    }
+}
+
+void leftArr(int (*cpyArr)[8], int x, int y, int n){
     
-    while(1){
-        // 방향 전환
-        if(!q.empty()){
-            if(q.front().first == t){
-                changeDir(q.front().second);
-                q.pop();
+    for(int i=y-1; i >=0 ; i--){
+        if(cpyArr[x][i] == 6) break;
+        if(cpyArr[x][i] >= 1 && cpyArr[x][i] <= 5) continue;
+        
+        cpyArr[x][i] = n;
+        
+    }
+}
+
+void rightArr(int (*cpyArr)[8], int x, int y, int n){
+    
+    for(int i=y+1; i < M ; i++){
+        if(cpyArr[x][i] == 6) break;
+        if(cpyArr[x][i] >= 1 && cpyArr[x][i] <= 5) continue;
+        
+        cpyArr[x][i] = n;
+        
+    }
+}
+
+void copyarr(int (*a)[8], int (*b)[8]){
+    
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            a[i][j] = b[i][j];
+        }
+    }
+}
+
+int calculation(int (*a)[8]){
+    int cnt = 0;
+    
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            //cout << a[i][j] << ' ';
+            if(a[i][j] == 0){
+                cnt+=1;
             }
         }
-        int nx = snake[snake.size()-1].first + dx[dir];
-        int ny = snake[snake.size()-1].second + dy[dir];
+        //cout <<'\n';
+    }
+    return cnt;
+}
+
+
+void solve(int (*arr)[8], int (*cpyArr)[8], int idx){
+    
+    if(idx == vt.size()){
+        ans = min(ans, calculation(cpyArr));
         
-        t++;
+     //   cout <<"\n";
+        return;
+    }
+    
+    int tmp[8][8];
+    
+    
+    if(idx> vt.size()) return;
+    
+    int i = vt[idx].first;
+    int j = vt[idx].second;
+    
+    if(arr[i][j] == 1 ){
+        // 상
+        /*   백 트레킹   */
+        /* 실행하기 전값을 저장해두고, 함수 종료후, 그 이전값을 다시 적용하기 */
+        copyarr(tmp, cpyArr);
+        upArr(cpyArr, i, j, -1);
         
-        // 벽에 부딪히거나, 자시자신과 부딪히면 종료
-        if((nx <1 || ny < 1 || nx > N || ny > N) || arr[nx][ny] == 1){
-            cout << t ;
-            return;
-        }
+        solve(arr, cpyArr, idx+1);
         
-        // 사과가 없다면 꼬리 줄이기
-        if(arr[nx][ny] != 2){
-            
-            // 벡터의 가장 처음이 뱀의 꼬리를 나타내므로 없애주기
-            arr[snake[0].first][snake[0].second] = 0;
-            snake.erase(snake.begin());
-        }
+        copyarr(cpyArr, tmp);
         
-        snake.push_back({nx, ny});
-        arr[nx][ny] = 1;
+        // 하
+        downArr(cpyArr, i, j, -1);
+    
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
         
+        // 좌
+        leftArr(cpyArr, i, j, -1);
+
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
+        
+        // 우
+        rightArr(cpyArr, i, j, -1);
+    
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
+        
+    }
+    else if(cpyArr[i][j] == 2){
+        copyarr(tmp, cpyArr);
+        upArr(cpyArr, i, j, -1);
+        downArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
+        
+        leftArr(cpyArr, i, j , -1);
+        rightArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx + 1);
+        
+        copyarr(cpyArr, tmp);
+
+        
+    }
+    else if(arr[i][j] == 3){
+        copyarr(tmp, cpyArr);
+        upArr(cpyArr, i, j, -1);
+        rightArr(cpyArr, i, j, -1);
+        
+        
+        solve(arr, cpyArr, idx + 1);
+        
+        copyarr(cpyArr, tmp);
+        
+        
+        leftArr(cpyArr, i, j, -1);
+        upArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
+        
+        leftArr(cpyArr, i, j, -1);
+        downArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
+        
+        rightArr(cpyArr, i, j, -1);
+        downArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
+        
+    }
+    else if(arr[i][j] == 4){
+        copyarr(tmp, cpyArr);
+        upArr(cpyArr, i, j, -1);
+        downArr(cpyArr, i, j, -1);
+        rightArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
+        
+        upArr(cpyArr, i, j, -1);
+        downArr(cpyArr, i, j, -1);
+        leftArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
+        
+        upArr(cpyArr, i, j, -1);
+        leftArr(cpyArr, i, j, -1);
+        rightArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx+1);
+        copyarr(cpyArr, tmp);
+        
+        downArr(cpyArr, i, j, -1);
+        leftArr(cpyArr, i, j, -1);
+        rightArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx+1);
+       copyarr(cpyArr, tmp);
+    }
+    else{
+        copyarr(tmp, cpyArr);
+        upArr(cpyArr, i, j, -1);
+        downArr(cpyArr, i, j, -1);
+        rightArr(cpyArr, i, j, -1);
+        leftArr(cpyArr, i, j, -1);
+        
+        solve(arr, cpyArr, idx+1);
     }
 }
 
-int main(){
-    int x, y, t;
-    char dir;
-    
-    cin >> N;
-    cin >> K;
-    for(int i=0; i<K; i++){
-        cin >> x>> y;
-        // 사과
-        arr[x][y] = 2;
-    }
-    
-    cin >> L;
-    for(int i=0; i<L; i++){
-        cin >> t >> dir;
-        
-        q.push({t, dir});
-    }
-    
-    // 뱀 표시
-    snake.push_back({1,1});
-    //snake.push_back({1,2});
-    arr[1][1] = 1; //arr[1][2] = 1;
-    
-    findsnakeTime();
 
+int main(){
     
+    int arr[8][8];
+    cin >> N >> M;
+    
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cin >> arr[i][j];
+            
+            if(arr[i][j] >= 1 && arr[i][j] <= 5){
+                vt.push_back({i, j});
+            }
+        }
+    }
+    
+    // copy
+    int cpyArr[8][8];
+    copyarr(cpyArr, arr);
+    
+    solve(arr, cpyArr, 0);
+    
+    cout << ans ;
     return 0;
 }
